2edc2cdb52299aa3d5f0bfc754c162fe72349f77
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2019-01-20 22:32:23 +0530

    Makeover Finished

--- js/main.js
+++ js/main.js
@@ -1,77 +1,147 @@
-$(document).ready(function() {
-    $('#main').hide()
-
-    // Intro animation
-
-    var container = $("#intro")
-        // RegEx identifies end of sentence
-    var _sentenceEndExp = /(\.|\?|!)$/g;
-
-    function machineGun(text) {
-
-        var words = text.split(" "),
-            tl = new TimelineMax({
-                delay: 1,
-                repeat: 0,
-                repeatDelay: 4
-            }),
-            wordCount = words.length,
-            time = 0,
-            word, element, duration, isSentenceEnd, i;
-
-        // For each word in the given string...
-        for (i = 0; i < wordCount; i++) {
-            word = words[i];
-            isSentenceEnd = _sentenceEndExp.test(word);
-
-            // Populate element variable with correct HTML
-            element = $("<h3>" + word + "</h3>").appendTo(container)
-
-            // Longer words stay on screen longer
-            duration = Math.max(0.5, word.length * 0.12)
+/* =================================
+------------------------------------
+	Civic - CV Resume
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
 
 
-            // Last word stays on the screen for longer
-            if (i == wordCount - 1) {
-                duration += 2
-                time += 0.8
-            }
 
-            // Long pause after each sentence
-            if (isSentenceEnd) {
-                duration += 0.8
-                time += 0.8
-            }
+'use strict';
 
-            TweenLite.set(element, {
-                autoAlpha: 0,
-                scale: 0,
-                z: 0.01
-            })
-            tl.to(element, duration, {
-                    scale: 1.2,
-                    ease: SlowMo.ease.config(0.25, 0.9)
-                }, time)
-                .to(element, duration, {
-                    autoAlpha: 1,
-                    ease: SlowMo.ease.config(0.25, 0.9, true)
-                }, time)
-            time += duration - 0.05
-        }
-    }
 
-    var timerBuffer = 0  // 15000
+$(window).on('load', function() { 
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
 
-    setTimeout(function() {
-        $('#intro').fadeOut(1000)
-    }, timerBuffer)
+});
 
-    setTimeout(function() {
-        $('#main').fadeIn(3000)
-        $('#top').hide().slideDown(3000).fadeIn(3000)
-        $('body').animate({
-            'backgroundColor': '#fff',
-            'color': '#000'
-        }, 500)
-    }, timerBuffer + 1000)
-})
+
+(function($) {
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	$('.review-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		items: 1,
+		autoplay: true
+	});
+
+
+
+	$('.progress-bar-style').each(function() {
+		var progress = $(this).data("progress");
+		var prog_width = progress + '%';
+		if (progress <= 100) {
+			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+		}
+		else {
+			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
+		}
+	});
+
+
+	$('.lan-prog').each(function() {
+		var progress = $(this).data("lanprogesss");
+		var ele      = '<span></span>';
+		var ele_fade = '<span class="fade-ele"></span>';
+		
+		for (var i = 1; i <= 5; i++) {
+			if(i <= progress){
+				$(this).append(ele);
+			} else {
+				$(this).append(ele_fade);
+			}
+		}
+	});
+
+
+	/*------------------
+		Popup
+	--------------------*/
+	$('.portfolio-item .port-pic').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 500,
+	});
+
+
+
+
+if($().circleProgress){
+
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 2,
+		fill: "#40424a",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 2,
+		fill: "#40424a",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle white
+	$("#progress3").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 2,
+		fill: "#ffffff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle white
+	$("#progress4").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 2,
+		fill: "#ffffff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle skyblue
+	$("#progress5").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 2,
+		fill: "#009fff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+	//Set progress circle skyblue
+	$("#progress6").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 2,
+		fill: "#009fff",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	$('.play-btn').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+}
+
+})(jQuery);
+
