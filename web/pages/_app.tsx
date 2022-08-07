05bd58de7297951dd1433a9c5885eb8a57dfff35
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-08-07 17:07:45 +0530

    feat: setup next-ui and themes

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { lightTheme, darkTheme } from '../src/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+	return (
+		<NextThemesProvider
+			defaultTheme="system"
+			attribute="class"
+			value={{
+				light: lightTheme.className,
+				dark: darkTheme.className
+			}}
+		>
+			<NextUIProvider>
+				<Component {...pageProps} />
+			</NextUIProvider>
+		</NextThemesProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
